ecff273279aa8e38d3f797932f01628103c73f4c
SAROJ KUMAR MANDAL <122289442+Sarojmandalgithub@users.noreply.github.com>
2023-08-09 19:48:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//get id and store const
+const animationBox = document.getElementById("animation-box");
+const animationControls = document.getElementById("animation-controls");
+
+
+function updateAnimation() {
+
+// get id store const
+  const animX = parseFloat(animationControls["anim-x"].value);
+  const animY = parseFloat(animationControls["anim-y"].value);
+  const animScale = parseFloat(animationControls["anim-scale"].value);
+  const animRotation = parseFloat(animationControls["anim-rotation"].value);
+  const animDuration = parseFloat(animationControls["anim-duration"].value);
+
+  animationBox.style.transform = `
+    translate(${animX}px, ${animY}px)
+    scale(${animScale})
+    rotate(${animRotation}deg)
+  `;
+
+  animationBox.style.transition = `transform ${animDuration}s ease-in-out`;
+}
+
+//onclick funtion update keliya using kiya hei 
+
+function changeHandler(event) {
+  updateAnimation();
+}
+
+updateAnimation();
